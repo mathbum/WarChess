3b5b8dc3c6f365dbef94335dc11848349d65e22b
Matthew Lash <mathbum112358@yahoo.com>
2016-01-01 10:46:52 -0800

    Added some objects and config file.

---
+++ WarChess/WarChess/Objects/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarChess.Objects {
+	public static class Config {
+		public static Dictionary<string, Unit> GoodUnits = new Dictionary<string, Unit> {
+			{ "Orc", new Unit("Orc",7,Allegiance.Evil,3,5,1,1,0,0,0)},
+			{ "Warrior",new Unit("Warrior",9,Allegiance.Good,3,5,1,1,0,0,0)}
+		};
+		public enum Allegiance { Good,Evil,Neutral};
+		public enum Phases { Priority,Move,Shoot,Fight};//need end phase?
+	}
+}
---
+++ WarChess/WarChess/Objects/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarChess.Objects {
+	public class Unit {
+		public Unit(string Name,int Points,Config.Allegiance Allegiance, int Strength,int Defense,int Attack,int Wounds,int Might,int Will,int Fate) {
+			this.Name = Name;
+			this.Points = Points;
+			this.Allegiance = Allegiance;
+			this.Strength = Strength;
+			this.Defense = Defense;
+			this.Attack = Attack;
+			this.Wounds = Wounds;
+			this.Might = Might;
+			this.Will = Will;
+			this.Fate = Fate;
+		}
+
+		public string Name { get; private set; }
+		public int Points { get; private set; }
+		public Config.Allegiance Allegiance { get; private set; }
+		//fighting? first is handtohand fighting second is minimum roll to land a hit with range
+		public int Strength { get; private set; }
+		public int Defense { get; private set; }
+		public int Attack { get; private set; }
+		public int Wounds { get; set; }
+		//public int Courage { get; set; }
+		public int Might { get; set; }
+		public int Will { get; set; }
+		public int Fate { get; set; }
+
+
+	}
+
+}
